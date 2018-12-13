141699f683cc2dda5192e57e2e6e7478c104acb3
Simón <simonxd81@gmail.com>
2018-12-13 01:15:19 -0300

    memes

--- ShintaroCommands.js
+++ ShintaroCommands.js
@@ -1,7 +1,15 @@
+
 const ytdl = require('ytdl-core');
 const streamOptions = {seek: 0, volume:1};
 const fs = require('fs');
 const { Client, RichEmbed } = require('discord.js');
+
+const request = require('request');
+
+let cheerio = require('cheerio')
+
+
+
 
 palanum = {
 	'si':'01',
@@ -54,25 +62,20 @@ function getCommand(comando,subcomando,msg,bot){
     switch(comando){ //la pos 0 es la palabra despues del uwu
 		case "loli":
 			msg.reply("Yoshi yoshi onichan")
-			.then(message => console.log(`Sent message: ${message.content}`))
+			.then(message => console.log(`Sent message: ${msg.content}`))
 			.catch(console.error);
 			break;
 		case "picture":
 			msg.reply("fillsquare.png")
-			.then(message => console.log(`Sent message: ${message.content}`))
+			.then(message => console.log(`Sent message: ${msg.content}`))
 			.catch(console.error);
 			break;
 		case "botqlo":
 			msg.reply('K wea otaku qlo bastardo ijo la comemoco')
-			.then(message => console.log(`Sent message: ${message.content}`))
+			.then(message => console.log(`Sent message: ${msg.content}`))
 			.catch(console.error);
 			break;
-		case "olo":
-			const canal = msg.channel
-			canal.send('hello!')
-			.then(message => console.log(`Sent message: ${message.content}`))
-			.catch(console.error);
-			break;
+		
 		case "yutu":
 			if(msg.member.voiceChannel){
 			const conexion = msg.member.voiceChannel.join()
@@ -90,16 +93,23 @@ function getCommand(comando,subcomando,msg,bot){
 		}
 			break;	
 		
-		case "embed":
-			const embed = new RichEmbed()
-			// Set the title of the field
-			.setTitle('A slick little embed')
-			// Set the color of the embed
-			.setColor(0xFF0000)
-			// Set the main content of the embed
-			.setDescription('Hello, this is a slick embed!');
-			// Send the embed to the same channel as the message
-			msg.channel.send(embed);
+		case "moneda":
+			if(Math.random() <.5 ){
+				msg.channel.send("Salio cara")
+				.then(message => console.log(`Sent message: ${msg.content}`))
+				.catch(console.error);
+			}
+			else{
+				msg.channel.send("Salio sello")
+				.then(message => console.log(`Sent message: ${msg.content}`))
+				.catch(console.error);
+			}
+			break;
+			
+		case "dado":
+			msg.channel.send(`Salio ${Math.floor(Math.random() * 6) + 1 }`)
+			.then(message => console.log(`Sent message: ${msg.content}`))
+			.catch(console.error);
 			break;
 		
 		case "age":
@@ -116,7 +126,65 @@ function getCommand(comando,subcomando,msg,bot){
 			else{
 				msg.reply("necesitas entrar a un canal de voz");
 			}
+
 			break;
+		case "agememe":
+			request('https://www.facebook.com/pg/Memes-hechos-con-Age-Of-Empires-2-Shitposting-226717164579337/', function (error, response, body) {
+			var $ = cheerio.load(body);
+				$('div._427x').each(function(i, element){
+					var b = $(this);  //no luck
+					var image = a.find('div._5cq3._1ktf').first('a._4-eo._2t9n').attr('href');
+					var image = b.find('a._4-eo._2t9n').attr('href'); 
+					console.log(image);
+						
+					msg.channel.send(`https://www.facebook.com${image}`)
+					.then(message => console.log(`Sent message: ${msg.content}`))
+					.catch(console.error);
+						
+						});
+
+					});
+					break;	
+		case "tiempo":
+			let apiKey = '2f1e8bb2fbf1116d0a9ceebfbb022766';
+			let city = `${subcomando}`;
+			let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
+			request(url, function (err, response, body) {
+			  if(err){
+				console.log('error:', error);
+			  } else {
+				let weather = JSON.parse(body)
+				let message = `Hay ${weather.main.temp}°C en ${weather.name}!
+							  La velocidad del viento es de ${weather.wind.speed} kilometros por hora y la humedad es de ${weather.main.humidity}%`;
+				console.log(message);
+				msg.reply(message);
+			  }
+			});
+			break;
+			
+		case "meme":
+			request('https://jaidefinichon.com/', function (error, response, body) {
+			var $ = cheerio.load(body);
+
+			$('div.image_post').each(function(i, element){
+				var a = $(this);
+				var image = a.find('figure').first('img').attr('src');  //no luck
+				var image = a.find('figure > img').attr('src'); 
+				console.log(image);
+				if(i > 1){
+					msg.channel.send(`${image}`)
+					.then(message => console.log(`Sent message: ${msg.content}`))
+					.catch(console.error);
+				}
+				});
+
+			});
+			break;
+			
+			
+		
+		
 		
 	}
 }
@@ -124,4 +192,5 @@ function getCommand(comando,subcomando,msg,bot){
 //exporto los comandos que se usaran
 module.exports = {
     getCommand,
+    
 }
