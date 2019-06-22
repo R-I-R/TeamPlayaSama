e7e28dbd65c138bfbcab535bc797ada244044c07
Esteban <esteban.naranjo.rayo@gmail.com>
2019-06-21 23:42:38 -0400

    aaaaa

--- youtube.js
+++ youtube.js
@@ -3,7 +3,7 @@ var OAuth2 = google.auth.OAuth2;
 
 
 const SCOPES = ['https://www.googleapis.com/auth/youtube'];
-const Auth = {"installed":{"client_id":"354767734095-91qtt338askol4dvk2nrplsu42a9mods.apps.googleusercontent.com","project_id":"teamplaya-sama","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://www.googleapis.com/oauth2/v3/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"-e-KQL-G-hRQJbZ6pXgyqOrn","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}};
+const Auth = {"installed":{"client_id":"354767734095-91qtt338askol4dvk2nrplsu42a9mods.apps.googleusercontent.com","project_id":"teamplaya-sama","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"ffctWgDLYrKv-4Ojr91e6chm","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
 const Credentials = {"access_token":"ya29.Glt3BjydA_iWFoQ0mi8mX8okmo7wxssyMqC_tmCtnVDit0FzRc6Le_pu_0fn5TY6CNw0r9s_B53hSdZ2Myod0ifBRRIqkwEqcL0gJrgbegGxSpbWGzWgrTU0a00v","refresh_token":"1/pp0kyKrnJUkMHhBR66efmcK8afAIyWsCkjiAq4psemzX1MYDhsXmLC2VddYQnpTS","scope":"https://www.googleapis.com/auth/youtube","token_type":"Bearer","expiry_date":1545194307101};
 
 
